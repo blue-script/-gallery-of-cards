b92ca75dab98e651fab923ff4a4fdb8b91a656d1
blue-script <Alexm92@inbox.ru>
2023-09-11 19:34:25 +0500

    initial

---
+++ app.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll('.slide')
+
+for (const slide of slides) {
+    slide.addEventListener('click', () => {
+        clearActiveCalsses()
+        slide.classList.add('active')
+    })
+}
+
+function clearActiveCalsses() {
+    slides.forEach((slide) => {
+        slide.classList.remove('active')
+    })
+}

3e36b026ae56ff56253a7e5405aeb658d115bf73
blue-script <Alexm92@inbox.ru>
2023-09-13 12:14:33 +0500

    add activeSlide

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const slides = document.querySelectorAll('.slide')
+function slidesPlugin(activeSlide = 0) {
+    const slides = document.querySelectorAll('.slide')
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveCalsses()
-        slide.classList.add('active')
-    })
+    slides[activeSlide].classList.add("active")
+
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveCalsses()
+            slide.classList.add('active')
+        })
+    }
+
+    function clearActiveCalsses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
 }
 
-function clearActiveCalsses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
-}
+slidesPlugin()
